836f4d3e24b3b2ccc7832f15a9410e4902277e14
s4iTen <ychouk102@gmail.com>
2023-04-13 05:02:05 +0200

    this is the last updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-from flask import Flask
+
 '''This is the Flask import'''
+
+
+from flask import Flask
 
 app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    """this is the """
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/', strict_slashes=False)
+def Hello():
+    """this is the index function"""
+    return "Hello HBNB!"
 
-    @app.route('/', strict_slashes=False)
-    def Hello():
-        """this is the index function"""
-        return "Hello HBNB!"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
