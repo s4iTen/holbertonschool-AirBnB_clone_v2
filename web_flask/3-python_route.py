a8a34461225fe989dc86779689176451d4e6ab86
s4iTen <ychouk102@gmail.com>
2023-04-13 10:38:00 +0200

    this function set a default value to a parameter variable of a function

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+'''This is the Flask import'''
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """this is the Hello function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def index():
+    """this is the index function"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """this function replace '_' with ' ' and concat it with the C char"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """this function replace '_' with ' ' and concat it with the C char"""
+    return "python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
