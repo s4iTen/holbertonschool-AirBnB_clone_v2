02c9f4f13d8048b9ca2eb14fe56909bd48ac4631
s4iTen <ychouk102@gmail.com>
2023-04-14 10:02:04 +0200

    this function changer the content of the html file the a variable

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+'''This is the Flask import'''
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """this is the Hello function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def index():
+    """this is the index function"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """this function replace '_' with ' ' and concat it with the C char"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """this function replace '_' with ' ' and concat it with the C char"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """this function display the n only if it's an integer"""
+    return (f'{n} is a number')
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
